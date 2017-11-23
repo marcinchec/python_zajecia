6709d9360bf6b13e8350a622270480891d8bae7e
Marcin Chęć <marcin.chec94@wp.pl>
2017-11-23 18:14:02 +0100

    szoste zajecia zadania

--- szoste_zajecia/piate.py
+++ szoste_zajecia/piate.py
@@ -16,4 +16,4 @@ for el in els:
         category = el1.getElementsByTagName("category")
         for x in range(len(category)):
             wynik= category[x].firstChild.data
-            print ("kategoria: %s") %(wynik)
+            print ("kategoria itemu: %s") %(wynik)
